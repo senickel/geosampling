0ed1729cf627a560d676912a94b0f38e18fd03f2
senickel <senickel@yahoo.de>
2018-09-12 14:00:30 +0200

    prepare_raster_of_sampling_area

---
+++ R/prepare_raster_of_sampling_area.R
@@ -0,0 +1,24 @@
+#' prepare_raster_of_sampling_area
+#'
+#' @param pop_raster
+#' @param area_polygon
+#' @keywords
+#' @keywords
+#' @export
+#' @examples
+#' @importFrom magrittr %>%
+#' @importFrom fasterize fasterize
+#'
+prepare_raster_of_sampling_area<-function(pop_raster,area_polygon) {
+
+  # cut raster to extent of border area
+  cropped_raster<-crop(pop_raster,area_polygon)
+
+  # convert polygon to raster
+  area_raster<-fasterize(area_polygon %>%
+                             st_as_sf,
+                           pop_raster)
+
+  # mask cropped_raster to area_raster
+  mask(cropped_raster,area_raster)
+}
